0440ab2286fc5e018df777b335a3e3ec38a86bd9
jordynrylander <jordynrylander@gmail.com>
2026-06-14 17:31:34 -0500

    Module 2.2 Rock Paper Scissors

---
+++ module-2/RockPaperScissors.java
@@ -0,0 +1,59 @@
+/*
+
+* Jordyn Rylander
+* MODULE 2.2
+* Rock Paper Scissors Game
+  */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        int computerChoice = random.nextInt(3) + 1;
+
+        System.out.println("Rock-Paper-Scissors");
+        System.out.println("1 = Rock");
+        System.out.println("2 = Paper");
+        System.out.println("3 = Scissors");
+
+        System.out.print("Enter your choice (1-3): ");
+        int userChoice = input.nextInt();
+
+        String computerMove = getMoveName(computerChoice);
+        String userMove = getMoveName(userChoice);
+
+        System.out.println("Computer chose: " + computerMove);
+        System.out.println("You chose: " + userMove);
+
+        if (userChoice < 1 || userChoice > 3) {
+            System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+        } else if (computerChoice == userChoice) {
+            System.out.println("It's a tie!");
+        } else if ((userChoice == 1 && computerChoice == 3) ||
+                   (userChoice == 2 && computerChoice == 1) ||
+                   (userChoice == 3 && computerChoice == 2)) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("Computer wins!");
+        }
+
+        input.close();
+    }
+
+    public static String getMoveName(int choice) {
+        switch (choice) {
+            case 1:
+                return "Rock";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+            default:
+                return "Invalid";
+        }
+    }
+}
